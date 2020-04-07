613ec92155c43dcd420a10426a920cc2075e621f
maksim.karpenko <maksim.karpenko@devexpress.com>
2020-04-07 15:01:12 +0300

    First commit

---
+++ CS/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using DevExpress.Xpo.Metadata;
+using PersistentClasses;
+
+namespace XpoSchemaMigration {
+    
+    class Program {
+
+        public const string ConnectionString = @"Server=(LocalDB)\MSSQLLocalDB;Integrated Security=true;MultipleActiveResultSets=true;initial catalog=schemamigration";
+
+        static void Main(string[] args) {
+
+            IDataStore provider = XpoDefault.GetConnectionProvider(ConnectionString, AutoCreateOption.DatabaseAndSchema);
+            var dataStoreSupportsMigration = (IDataStoreSupportSchemaMigration)provider;
+            var migrationScriptFormatter = (IUpdateSchemaSqlFormatter)provider;
+
+            var dictionary = new ReflectionDictionary();
+            DBTable[] targetSchema = dictionary.GetDataStoreSchema(typeof(Customer), typeof(Order), typeof(Product));
+
+            var migrationOptions = new SchemaMigrationOptions();
+            var updateSchemaStatements = dataStoreSupportsMigration.CompareSchema(targetSchema, migrationOptions);
+            string sql = migrationScriptFormatter.FormatUpdateSchemaScript(updateSchemaStatements);
+
+            Console.WriteLine("SQL Script to database schema migration:");
+            Console.WriteLine("-----begin script------");
+            Console.WriteLine(sql);
+            Console.WriteLine("------end script-------");
+        }
+    }
+}
+
+namespace PersistentClasses { 
+
+    class Customer : XPObject {
+        public string CompanyName;     
+       
+        [Size(11)]
+        public string Phone;
+
+        [Association("customer-order")]
+        public XPCollection<Order> Orders {
+            get {
+                return GetCollection<Order>();
+            }
+        }
+    }
+
+    class Order: XPLiteObject {
+        [Key(true)]
+        public int Id;
+        
+        [Association("customer-order")]
+        public Customer Customer;
+
+        [Association("product-order")]
+        public XPCollection<Product> Products {
+            get {
+                return GetCollection<Product>();
+            }
+        }
+    }
+    
+    class Product : XPLiteObject {
+        [Key(false)]
+        [Size(10)]
+        public string ProductCode;
+
+        public string ProductName;
+
+        public decimal UnitPrice;
+
+        [Association("product-order")]
+        public XPCollection<Order> Orders {
+            get {
+                return GetCollection<Order>();
+            }
+        }
+
+        public Product(Session session) : base(session) { }
+    }
+}
---
+++ VB/Program.vb
@@ -0,0 +1,92 @@
+﻿Option Infer On
+
+Imports System
+Imports DevExpress.Xpo
+Imports DevExpress.Xpo.DB
+Imports DevExpress.Xpo.Metadata
+Imports XpoSchemaMigration.PersistentClasses
+
+Namespace XpoSchemaMigration
+
+    Friend Class Program
+
+        Public Const ConnectionString As String = "Server=(LocalDB)\MSSQLLocalDB;Integrated Security=true;MultipleActiveResultSets=true;initial catalog=schemamigration"
+
+        Shared Sub Main(ByVal args() As String)
+
+            Dim provider As IDataStore = XpoDefault.GetConnectionProvider(ConnectionString, AutoCreateOption.DatabaseAndSchema)
+            Dim dataStoreSupportsMigration = DirectCast(provider, IDataStoreSupportSchemaMigration)
+            Dim migrationScriptFormatter = DirectCast(provider, IUpdateSchemaSqlFormatter)
+
+            Dim dictionary = New ReflectionDictionary()
+            Dim targetSchema() As DBTable = dictionary.GetDataStoreSchema(GetType(Customer), GetType(Order), GetType(Product))
+
+            Dim migrationOptions = New SchemaMigrationOptions()
+            Dim updateSchemaStatements = dataStoreSupportsMigration.CompareSchema(targetSchema, migrationOptions)
+            Dim sql As String = migrationScriptFormatter.FormatUpdateSchemaScript(updateSchemaStatements)
+
+            Console.WriteLine("SQL Script to database schema migration:")
+            Console.WriteLine("-----begin script------")
+            Console.WriteLine(sql)
+            Console.WriteLine("------end script-------")
+        End Sub
+    End Class
+End Namespace
+
+Namespace PersistentClasses
+
+    Friend Class Customer
+        Inherits XPObject
+
+        Public CompanyName As String
+
+        <Size(11)>
+        Public Phone As String
+
+        <Association("customer-order")>
+        Public ReadOnly Property Orders() As XPCollection(Of Order)
+            Get
+                Return GetCollection(Of Order)()
+            End Get
+        End Property
+    End Class
+
+    Friend Class Order
+        Inherits XPLiteObject
+
+        <Key(True)>
+        Public Id As Integer
+
+        <Association("customer-order")>
+        Public Customer As Customer
+
+        <Association("product-order")>
+        Public ReadOnly Property Products() As XPCollection(Of Product)
+            Get
+                Return GetCollection(Of Product)()
+            End Get
+        End Property
+    End Class
+
+    Friend Class Product
+        Inherits XPLiteObject
+
+        <Key(False), Size(10)>
+        Public ProductCode As String
+
+        Public ProductName As String
+
+        Public UnitPrice As Decimal
+
+        <Association("product-order")>
+        Public ReadOnly Property Orders() As XPCollection(Of Order)
+            Get
+                Return GetCollection(Of Order)()
+            End Get
+        End Property
+
+        Public Sub New(ByVal session As Session)
+            MyBase.New(session)
+        End Sub
+    End Class
+End Namespace
